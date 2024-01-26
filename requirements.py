3195f35e546fe496405df56614abffebcc2dc219
pedromascaro <157767744+pedromascaro@users.noreply.github.com>
2024-01-26 08:17:37 -0300

    Update and rename requirements.txt to requirements.py

---
+++ requirements.py
@@ -0,0 +1,15 @@
+Imports for the openpyxl package.
+from openpyxl.xml import LXML
+
+from openpyxl.workbook import Workbook
+from openpyxl.reader.excel import load_workbook
+
+
+# constants
+__version__ = '2.2.4'
+
+__author__ = 'Eric Gazoni'
+__license__ = 'MIT/Expat'
+__author_email__ = 'eric.gazoni@gmail.com'
+__maintainer_email__ = 'openpyxl-users@googlegroups.com'
+__url__ = 'http://openpyxl.readthedocs.org'
